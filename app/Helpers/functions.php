0cf7d835f4b0186f4de9fb0ad9db5728df75dd5b
gitadmin <111111>
2018-04-25 21:32:21 +0800

    1

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,5 +1,10 @@
 <?php
-function showUEditor($name,$content='',$toolbars='all'){
+/**
+ * @param string $name textarea文本域的name
+ * @param string $content 默认值
+ * @param string $toolbars 工具栏
+ */
+function showUEditor(string $name,$content='',$toolbars='all'){
     if ($toolbars == 'all') {
         $toolbars = <<<EOP
             toolbars: [[
@@ -33,7 +38,8 @@ EOP;
                 ]]
 EOP;
     }
-    //定义一个静态变量，用于判断是否是第一次加载，因为配置文件以及编辑器源码文件只能加载一次
+    //定义一个静态变量为false，表示未加载，
+    //用于判断是否是第一次加载，因为配置文件以及编辑器源码文件只能加载一次
     static $isLodeScript = false;
     //加载编辑器的容器
     $str = '<script id="'.$name.'" name="'.$name.'" type="text/plain">';
@@ -47,6 +53,7 @@ EOP;
     $str.= '<script type="text/javascript">';
     $str.= 'var ue_'.$name.' = UE.getEditor("'.$name.'",{'.$toolbars.',initialFrameWidth:"99%"});';
     $str.= '</script>';
+    //设置为true表示已经加载
     $isLodeScript = true;
     echo $str;
 }
