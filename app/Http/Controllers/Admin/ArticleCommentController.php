f0b33e8ca25e992a147aff9da7921ce34946fe3d
gitadmin <111111>
2018-04-29 10:00:18 +0800

    1

---
+++ app/Http/Controllers/Admin/ArticleCommentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Model\ArticleComment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticleCommentController extends Controller
+{
+    //列表
+    public function showList(Request $request){
+        if (\Route::input('action')){
+            //返回数据格式
+            $return =array('code'=>0,'msg'=>'','count'=>0,'data'=>array());
+            $whereArray = array();
+            $orWhereArray = array();
+            if ($request->input('startTime')){
+                $whereArray[] = ['time','>=',strtotime($request->input('startTime'))];
+            }
+            if ($request->input('endTime')){
+                $whereArray[] = ['time','<=',strtotime($request->input('endTime'))+86400];
+            }
+            if ($request->input('keyWord')){
+                $orWhereArray[] = ['article_name','like','%'.$request->input('keyWord').'%'];
+                $orWhereArray[] = ['user_account','like','%'.$request->input('keyWord').'%'];
+            }
+            $return['count'] = ArticleComment::where($whereArray)
+                ->where(function ($query) use ($orWhereArray){
+                    foreach ($orWhereArray as $item) {
+                        $query->orWhere($item[0],$item[1],$item[2]);
+                    }
+                })
+                ->count();
+            $return['data'] = ArticleComment::where($whereArray)
+                ->where(function ($query) use ($orWhereArray){
+                    foreach ($orWhereArray as $item) {
+                        $query->orWhere($item[0],$item[1],$item[2]);
+                    }
+                })
+                ->orderBy('time','desc')
+                ->paginate($request->input('limit'))
+                ->toArray()['data'];
+            exit(json_encode($return));
+        }
+        return view('Admin.ArticleComment.showList');
+    }
+
+    //删除
+    public function ajaxDel(){
+        $res = array(
+            'status' => false,
+            'echo'  => ''
+        );
+        $ids = $_GET['id'];
+        $ids = trim($ids,',');
+        ArticleComment::destroy(explode(',',$ids));
+        $res['status'] = true;
+        $res['echo'] = '删除成功';
+        exit(json_encode($res));
+    }
+}
