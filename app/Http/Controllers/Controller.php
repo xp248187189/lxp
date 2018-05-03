57427eeac44376cb540ae8f5b183d62af5de6ed7
company <company@qq.com>
2018-05-03 15:58:50 +0800

    1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        //请求方式
+        $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https' : 'http';
+        if ($http_type == 'http'){
+            $length = 7;
+        }else{
+            $length = 8;
+        }
+        $current_url = url()->full();
+        $str = substr($current_url,$length,3);
+        if ($str != 'www'){
+            $url_arr = explode('://',$current_url);
+            $url = $http_type.'://www.'.$url_arr[1];
+            $this->middleware(function() use ($url){
+                return redirect($url,301);
+            });
+        }
+    }
 }
