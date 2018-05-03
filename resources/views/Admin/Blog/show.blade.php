1a85181d105796beea3a7ff618a1ff7ca399fe9b
company <company@qq.com>
2018-05-03 17:08:35 +0800

    1

--- resources/views/Admin/Blog/show.blade.php
+++ resources/views/Admin/Blog/show.blade.php
@@ -24,26 +24,26 @@
                 <input type="text" name="introduce" lay-verify="required" autocomplete="off" class="layui-input" value="{{$info->introduce}}">
             </div>
         </div>
-        <div class="layui-form-item">
-            <label class="layui-form-label">主图</label>
-            <div class="layui-input-inline">
-                <button type="button" onclick="selImg();" class="layui-btn layui-btn-sm" style="margin-top: 4px;">
-                    <i class="fa fa-image"></i>
-                    浏览文件
-                </button>
-                <input type="file" name="img" id="img" style="display:none;">
-                <input type="hidden" id="checkCardPath" value="1" />
-            </div>
-            <div class="layui-form-mid layui-word-aux" id="showPath">
+        {{--<div class="layui-form-item">--}}
+            {{--<label class="layui-form-label">主图</label>--}}
+            {{--<div class="layui-input-inline">--}}
+                {{--<button type="button" onclick="selImg();" class="layui-btn layui-btn-sm" style="margin-top: 4px;">--}}
+                    {{--<i class="fa fa-image"></i>--}}
+                    {{--浏览文件--}}
+                {{--</button>--}}
+                {{--<input type="file" name="img" id="img" style="display:none;">--}}
+                {{--<input type="hidden" id="checkCardPath" value="1" />--}}
+            {{--</div>--}}
+            {{--<div class="layui-form-mid layui-word-aux" id="showPath">--}}
 
-            </div>
-        </div>
-        <div class="layui-form-item">
-            <label class="layui-form-label"></label>
-            <div class="layui-input-block">
-                <img style="width:150px;" id="show_img" src="@php echo asset('uploads').'/'.$info->img@endphp">
-            </div>
-        </div>
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="layui-form-item">--}}
+            {{--<label class="layui-form-label"></label>--}}
+            {{--<div class="layui-input-block">--}}
+                {{--<img style="width:150px;" id="show_img" src="@php echo asset('uploads').'/'.$info->img@endphp">--}}
+            {{--</div>--}}
+        {{--</div>--}}
         <div class="layui-form-item">
             <label class="layui-form-label">详情</label>
             <div class="layui-input-block">
