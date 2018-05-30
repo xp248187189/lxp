e723fb82cc4635a05d47c7aab7a4dea23e5143a2
company <company@qq.com>
2018-05-30 17:23:58 +0800

    1

--- app/Http/Controllers/Home/TimeAxisController.php
+++ app/Http/Controllers/Home/TimeAxisController.php
@@ -6,36 +6,49 @@ use App\Model\About;
 use App\Model\TimeAxis;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class TimeAxisController extends Controller
 {
-    public function timeAxis(){
-        $year = TimeAxis::where('status','=','1')
-            ->orderBy('year','desc')
-            ->groupBy('year')
-            ->select('year')
-            ->get();
-        foreach ($year as $k => $v){
-            $year[$k]['zi'] = TimeAxis::where('status','=','1')
-                ->where('year','=',$v['year'])
-                ->orderBy('month','desc')
-                ->groupBy('month')
-                ->select('month')
+    public function timeAxis(Request $request){
+        $year = Cache::remember(sha1($request->fullUrl().'_year_cache'),10,function (){
+            return TimeAxis::where('status','=','1')
+                ->orderBy('year','desc')
+                ->groupBy('year')
+                ->select('year')
                 ->get();
-            foreach ($year[$k]['zi'] as $kk => $vv) {
-                $year[$k]['zi'][$kk]['zi'] = TimeAxis::where('status','=','1')
+        });
+        foreach ($year as $k => $v){
+            $year[$k]['zi'] = Cache::remember(sha1($request->fullUrl().'_year_'.$k.'_zi_cache'),10,function () use ($v){
+                return TimeAxis::where('status','=','1')
                     ->where('year','=',$v['year'])
-                    ->where('month','=',$vv['month'])
-                    ->orderBy('day','desc')
+                    ->orderBy('month','desc')
+                    ->groupBy('month')
+                    ->select('month')
                     ->get();
+            });
+            foreach ($year[$k]['zi'] as $kk => $vv) {
+                $year[$k]['zi'][$kk]['zi'] = Cache::remember($request->fullUrl().'_year_'.$k.'_zi_'.$kk.'_zi_cache',10,function () use ($v,$vv){
+                    return TimeAxis::where('status','=','1')
+                        ->where('year','=',$v['year'])
+                        ->where('month','=',$vv['month'])
+                        ->orderBy('day','desc')
+                        ->get();
+                });
             }
         }
         //关于博客
-        $blogInfo = About::find(2);
+        $blogInfo = Cache::remember(sha1($request->fullUrl().'_blogInfo_cache'),10,function (){
+            return About::find(2);
+        });
         //关键字
-        $keyWordsInfo = About::find(3);
+        $keyWordsInfo = Cache::remember(sha1($request->fullUrl().'_keyWordsInfo_cache'),10,function (){
+            return About::find(3);
+        });
         //描述
-        $descriptionInfo = About::find(4);
+        $descriptionInfo = Cache::remember(sha1($request->fullUrl().'_descriptionInfo_cache'),10,function (){
+            return About::find(4);
+        });
         return view('Home.TimeAxis.timeAxis')->with('timeAxisList',$year)
             ->with('blogInfo',$blogInfo)
             ->with('keyWordsInfo',$keyWordsInfo)
