537fedc92c41561e63eb757dd3794fae30122c09
gitadmin <111111>
2018-06-10 10:15:16 +0800

    1

--- app/Http/Controllers/Admin/NoteController.php
+++ app/Http/Controllers/Admin/NoteController.php
@@ -35,10 +35,10 @@ class NoteController extends Controller
     //执行添加
     public function ajaxAdd(Request $request){
         $orm = new Note();
-        $orm->title = is_null($request->input('title'))?'':$request->input('title');
-        $orm->url = is_null($request->input('url'))?'':$request->input('url');
-        $orm->account = is_null($request->input('account'))?'':$request->input('account');
-        $orm->password = is_null($request->input('password'))?'':$request->input('password');
+        $orm->title = $request->filled('title')?$request->title:'';
+        $orm->url = $request->filled('url')?$request->url:'';
+        $orm->account = $request->filled('account')?$request->account:'';
+        $orm->password = $request->filled('password')?$request->password:'';
         $orm->save();
         $res['status'] = true;
         $res['echo'] = '添加成功';
@@ -56,10 +56,10 @@ class NoteController extends Controller
     public function ajaxEdit(Request $request){
         $id = $request->input('id');
         $orm = Note::find($id);
-        $orm->title = is_null($request->input('title'))?'':$request->input('title');
-        $orm->url = is_null($request->input('url'))?'':$request->input('url');
-        $orm->account = is_null($request->input('account'))?'':$request->input('account');
-        $orm->password = is_null($request->input('password'))?'':$request->input('password');
+        $orm->title = $request->filled('title')?$request->title:'';
+        $orm->url = $request->filled('url')?$request->url:'';
+        $orm->account = $request->filled('account')?$request->account:'';
+        $orm->password = $request->filled('password')?$request->password:'';
         $orm->save();
         $res['status'] = true;
         $res['echo'] = '修改成功';
