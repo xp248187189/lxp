b9c38c9303dac9c23a1d3c94c8515039e0418cdd
company <company@qq.com>
2018-06-29 14:08:33 +0800

    1

--- app/Http/Controllers/Home/JokeController.php
+++ app/Http/Controllers/Home/JokeController.php
@@ -24,7 +24,7 @@ class JokeController extends Controller
             return About::find(4);
         });
         //归档列表
-        $list = Cache::remember(sha1($request->fullUrl().'_jokeList_cache'),10,function (){
+        $list = Cache::remember(sha1($request->fullUrl().'_jokeList_cache'),15,function (){
             return json_decode(curl('http://v.juhe.cn/joke/randJoke.php',['key'=>config('juheApi.juhe_key')]));
         });
         return view('Home.Joke.joke')->with('jokeList',$list->result)
