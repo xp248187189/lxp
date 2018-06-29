07cc0ef6254cc121dee0cfedc249a7e714bf087e
company <company@qq.com>
2018-06-29 15:47:13 +0800

    1

--- app/Http/Controllers/Home/ArchiveController.php
+++ app/Http/Controllers/Home/ArchiveController.php
@@ -12,17 +12,17 @@ class ArchiveController extends Controller
 {
     public function archive(Request $request){
         //归档列表
-        $list = Cache::remember(sha1($request->fullUrl().'_archiveList_cache'),10,function (){
-             return Article::where('status','=','1')
+        $new_list = Cache::remember(sha1($request->fullUrl().'_archiveList_cache'),10,function (){
+             $list = Article::where('status','=','1')
                 ->orderBy('created_at','desc')
                 ->select('id','title','addDate')
                 ->get()
                 ->toArray();
+            foreach ($list as $key => $value){
+                $list[$key]['addYearMonth'] = substr($value['addDate'],0,7);
+            }
+            return arrayGroupBy($list,'addYearMonth');
         });
-        foreach ($list as $key => $value){
-            $list[$key]['addYearMonth'] = substr($value['addDate'],0,7);
-        }
-        $new_list = arrayGroupBy($list,'addYearMonth');
         return view('Home.Archive.archive')->with('archiveList',$new_list)
             ->with('controllerName','Archive');
     }
