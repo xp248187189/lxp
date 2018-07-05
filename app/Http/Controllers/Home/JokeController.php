06f6f9a340da163914e78662506ada88d54175cf
company <company@qq.com>
2018-07-05 17:55:45 +0800

    1

--- app/Http/Controllers/Home/JokeController.php
+++ app/Http/Controllers/Home/JokeController.php
@@ -11,10 +11,14 @@ use Illuminate\Support\Facades\Cache;
 class JokeController extends Controller
 {
     public function joke(Request $request){
-        //归档列表
+        //笑话列表
         $list = Cache::remember(sha1($request->fullUrl().'_jokeList_cache'),15,function (){
             return json_decode(curl('http://v.juhe.cn/joke/randJoke.php',['key'=>config('juheApi.juhe_key')]));
         });
-        return view('Home.Joke.joke')->with('jokeList',$list->result);
+        //历史上的今天
+        $todayonhistory = Cache::remember(sha1($request->fullUrl().'_todayonhistory_cache'),15,function (){
+            return json_decode(curl('https://www.ipip5.com/today/api.php',['type'=>'json'],false,true));
+        });
+        return view('Home.Joke.joke')->with('jokeList',$list->result)->with('todayonhistory',$todayonhistory);
     }
 }
--- resources/views/Home/Joke/joke.blade.php
+++ resources/views/Home/Joke/joke.blade.php
@@ -11,6 +11,20 @@
             position: relative;
             min-height: 90vh;
         }
+        .layui-tab-brief {
+            background: #fff;
+            min-height: 100vh;
+        }
+        .layui-tab-brief .layui-tab-title {
+            text-align: center;
+            border-bottom: 1px solid #5FB878;
+        }
+        .layui-tab-brief .layui-tab-title li {
+            font-size: 12px;
+        }
+        .layui-tab-brief .layui-tab-content {
+            padding: 0;
+        }
     </style>
 @endsection
 {{--body内容--}}
@@ -23,25 +37,72 @@
                 <a><cite>轻松一刻</cite></a>
             </blockquote>
             <div class="blog-main">
-                <div class="timeline-box shadow">
-                    <ul class="layui-timeline">
-                        @foreach($jokeList as $key => $value)
-                        <li class="layui-timeline-item">
-                            <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
-                            <div class="layui-timeline-content layui-text">
-                                {!! str_replace(PHP_EOL,'<br/>',$value->content) !!}
-                            </div>
-                        </li>
-                        @endforeach
-                        <li class="layui-timeline-item">
-                            <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
-                            <div class="layui-timeline-content layui-text">
-                                <h3 class="layui-timeline-title">END</h3>
-                            </div>
-                        </li>
+                <div class="layui-tab layui-tab-brief shadow" lay-filter="tabAbout">
+                    <ul class="layui-tab-title">
+                        <li class="layui-this" lay-id="1">开心一笑</li>
+                        <li lay-id="2">历史上的今天</li>
                     </ul>
+                    <div class="layui-tab-content">
+                        <div class="layui-tab-item layui-show">
+                            <div class="timeline-box shadow">
+                                <ul class="layui-timeline">
+                                    @foreach($jokeList as $key => $value)
+                                        <li class="layui-timeline-item">
+                                            <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+                                            <div class="layui-timeline-content layui-text">
+                                                <p>{!! str_replace(PHP_EOL,'<br/>',$value->content) !!}</p>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                    <li class="layui-timeline-item">
+                                        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+                                        <div class="layui-timeline-content layui-text">
+                                            <h3 class="layui-timeline-title">END</h3>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="layui-tab-item">
+                            <div class="timeline-box shadow">
+                                <ul class="layui-timeline">
+                                    @foreach($todayonhistory->result as $key => $value)
+                                        <li class="layui-timeline-item">
+                                            <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+                                            <div class="layui-timeline-content layui-text">
+                                                <h3 class="layui-timeline-title">{{$value->year}}年{{$todayonhistory->today}}</h3>
+                                                <p>{{$value->title}}</p>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                    <li class="layui-timeline-item">
+                                        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+                                        <div class="layui-timeline-content layui-text">
+                                            <h3 class="layui-timeline-title">END</h3>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+{{--js内容--}}
+@section('script')
+    <script type="text/javascript">
+    {{--Hash地址的定位--}}
+    var layid = location.hash.replace(/^#tabIndex=/, '');
+    if (layid == "") {
+        element.tabChange('tabAbout', 1);
+    }
+    {{--切换--}}
+    element.tabChange('tabAbout', layid);
+    {{--更改hash--}}
+    element.on('tab(tabAbout)', function (elem) {
+        location.hash = 'tabIndex=' + $(this).attr('lay-id');
+    });
+</script>
 @endsection
