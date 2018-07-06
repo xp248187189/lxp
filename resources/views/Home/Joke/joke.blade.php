4fad7b11b7cd8ec1607f557ed8839b5cbf809d4f
company <company@qq.com>
2018-07-06 14:11:58 +0800

    1

--- resources/views/Home/Joke/joke.blade.php
+++ resources/views/Home/Joke/joke.blade.php
@@ -29,7 +29,6 @@
 @endsection
 {{--body内容--}}
 @section('body')
-    <script type="text/javascript">(function(){document.write(unescape('%3Cdiv id="bdcs"%3E%3C/div%3E'));var bdcs = document.createElement('script');bdcs.type = 'text/javascript';bdcs.async = true;bdcs.src = 'http://znsv.baidu.com/customer_search/api/js?sid=2739665532248753940' + '&plate_url=' + encodeURIComponent(window.location.href) + '&t=' + Math.ceil(new Date()/3600000);var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(bdcs, s);})();</script>
     {{--主体（一般只改变这里的内容）--}}
     <div class="blog-body">
         <div class="blog-container">
