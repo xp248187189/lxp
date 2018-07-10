a673a5f33a6297c3fc13cce41103b94ecca47a91
company <company@qq.com>
2018-07-10 16:35:39 +0800

    1

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -330,3 +330,28 @@ function timeAgo(string $paramDate){
     }
     return $paramDate;
 }
+function zipDir($basePath,$zipName){
+    $zip = new ZipArchive();
+    $fileArr = [];
+    $fileNum = 0;
+    if (is_dir($basePath)){
+        if ($dh = opendir($basePath)){
+            $zip->open($zipName,ZipArchive::CREATE);
+            while (($file = readdir($dh)) !== false){
+                if(in_array($file,['.','..',])) continue; //无效文件，重来
+                $file = iconv('gbk','utf-8',$file);
+                $extension = strchr($file,'.');
+                rename(iconv('UTF-8','GBK',$basePath.'\\'.$file), iconv('UTF-8','GBK',$basePath.'\\'.$fileNum.$extension));
+                $zip->addFile($basePath.'\\'.$fileNum.$extension,$fileNum.$extension);
+                $zip->renameName($fileNum.$extension,$file);
+                $fileArr[$fileNum.$extension] = $file;
+                $fileNum++;
+            }
+            $zip->close();
+            closedir($dh);
+            foreach($fileArr as $k=>$v){
+                rename(iconv('UTF-8','GBK',$basePath.'\\'.$k), iconv('UTF-8','GBK',$basePath.'\\'.$v));
+            }
+        }
+    }
+}
