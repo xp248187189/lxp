86ed67208849b0471a1c88c703ac8ba8731ba0f6
company <company@qq.com>
2018-07-20 12:31:45 +0800

    1

--- config/api.php
+++ config/api.php
@@ -11,6 +11,9 @@ return [
     'qq_app_key' => env('QQ_APP_KEY'),
     'qq_call_back' => env('QQ_CALL_BACK'),
     'qq_scope' => env('QQ_SCOPE'),
+    //易源数据
+    'showapi_appid' => env('SHOWAPI_APPID'),
+    'showapi_sign' => env('SHOWAPI_SIGN'),
     //随机获取图片
     'getImgApi' => [
         ['title'=>'二次元动漫','url'=>'https://api.ixiaowai.cn/api/api.php'],
