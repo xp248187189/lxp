d803ff3dc5ad7b6f41e1b811f99585dc915fa53c
gitadmin <111111>
2018-10-10 15:21:21 +0800

    1

--- app/Http/Controllers/Home/JokeController.php
+++ app/Http/Controllers/Home/JokeController.php
@@ -11,16 +11,16 @@ use Illuminate\Support\Facades\Cache;
 class JokeController extends Controller
 {
     public function joke(Request $request){
-        $data = Cache::remember(sha1($request->fullUrl()),20,function (){
-            //笑话列表
-            $list = json_decode(curl('http://v.juhe.cn/joke/randJoke.php',['key'=>config('api.juhe_key')]));
-            //历史上的今天
-            $todayonhistory = json_decode(curl('https://www.ipip5.com/today/api.php',['type'=>'json'],false,true));
-            $data = new \stdClass();
-            $data->jokeList = $list->result;
-            $data->todayonhistory = $todayonhistory;
-            return $data;
-        });
+        //$data = Cache::remember(sha1($request->fullUrl()),20,function (){
+        //    //笑话列表
+        //    $list = json_decode(curl('http://v.juhe.cn/joke/randJoke.php',['key'=>config('api.juhe_key')]));
+        //    //历史上的今天
+        //    $todayonhistory = json_decode(curl('https://www.ipip5.com/today/api.php',['type'=>'json'],false,true));
+        //    $data = new \stdClass();
+        //    $data->jokeList = $list->result;
+        //    $data->todayonhistory = $todayonhistory;
+        //    return $data;
+        //});
         $todayonhistory = json_decode(curl('https://www.ipip5.com/today/api.php',['type'=>'json'],false,true));
         dd($todayonhistory);
         //return view('Home.Joke.joke')->with('data',$data);
