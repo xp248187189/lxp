4cfc3528d563d99cfce87c1ba6a4bb6323e4518b
gitadmin <111111>
2018-10-10 15:52:04 +0800

    1

--- app/Http/Controllers/Home/JokeController.php
+++ app/Http/Controllers/Home/JokeController.php
@@ -21,8 +21,6 @@ class JokeController extends Controller
             $data->todayonhistory = $todayonhistory;
             return $data;
         });
-        $a = curl('https://www.ipip5.com/today/api.php?type=json',false,false,true);
-        dd($a);
         return view('Home.Joke.joke')->with('data',$data);
     }
 }
