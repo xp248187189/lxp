50db61d4d2f3eb3d8ee6397ef21e65b7e18febe7
user <248187189@qq.com>
2018-12-03 16:23:09 +0800

    1

---
+++ resources/views/Home/BingImg/atlas.blade.php
@@ -0,0 +1,50 @@
+{{--继承模板--}}
+@extends('Home.Public.public')
+{{--设置title--}}
+@section('title', cache('about_cache')[1]->name.' - 高清壁纸')
+@section('loadCss')
+    <link rel="stylesheet" type="text/css" href="{{ asset('Common/css/page.css') }}">
+@endsection
+{{--设置样式--}}
+@section('style')
+    <style>
+        .timeline-box {
+            background: #fff;
+            padding: 8px;
+            position: relative;
+            min-height: 90vh;
+        }
+    </style>
+@endsection
+{{--body内容--}}
+@section('body')
+    {{--主体（一般只改变这里的内容）--}}
+    <div class="blog-body">
+        <div class="blog-container">
+            <blockquote class="layui-elem-quote sitemap layui-breadcrumb shadow">
+                <a href="{{url('/')}}" title="网站首页">网站首页</a>
+                <a><cite>高清壁纸</cite></a>
+            </blockquote>
+            <div class="blog-main">
+                <div class="timeline-box shadow">
+                    <div class="layui-row">
+                        @foreach($data as $key => $value)
+                        <div class="layui-col-sm3">
+                            <div style="padding: 10px;">
+                                <img lay-src="{{$value->url}}" width="100%">
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                {{$data->links()}}
+            </div>
+        </div>
+    </div>
+@endsection
+{{--js内容--}}
+@section('script')
+    <script type="text/javascript">
+        flow.lazyimg();
+    </script>
+@endsection
