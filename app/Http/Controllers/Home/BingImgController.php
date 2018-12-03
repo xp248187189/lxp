d042fe0a70b1ecdc6e572fee3c9c4d1aaed362aa
user <248187189@qq.com>
2018-12-03 17:30:06 +0800

    1

--- app/Http/Controllers/Home/BingImgController.php
+++ app/Http/Controllers/Home/BingImgController.php
@@ -10,13 +10,18 @@ use App\Http\Controllers\Controller;
 class BingImgController extends Controller
 {
     public function atlas(Request $request){
-        $data = Cache::remember(sha1($request->fullUrl().'_atlas_cache'),1,function (){
-            return BingImg::orderBy('date','desc')
-                ->paginate(20,['*'],'p');
-        });
-        if ($data->isEmpty()){
-            return redirect('/atlas');
+        if (\Route::input('action') == 'getData'){
+            $data = Cache::remember(sha1($request->fullUrl().'_atlas_cache'),10,function (){
+                return BingImg::orderBy('date','desc')
+                    ->paginate(20,['*'],'page')
+                    ->toArray();
+            });
+            $returnData = [
+                'data' => $data['data'],
+                'pages' => $data['last_page']
+            ];
+            return $returnData;
         }
-        return view('Home.BingImg.atlas')->with('data',$data);
+        return view('Home.BingImg.atlas');
     }
 }
