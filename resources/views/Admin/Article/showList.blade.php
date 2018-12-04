0b195033922cea82fd735beabdc9b442517bdebf
user <248187189@qq.com>
2018-12-04 13:28:29 +0800

    1

--- resources/views/Admin/Article/showList.blade.php
+++ resources/views/Admin/Article/showList.blade.php
@@ -103,7 +103,13 @@
             }
             $.post('{{url("/Article/ajaxEdit")}}',{id:data.value,status:statusVal},function(result){
                 layer.msg(result.echo);
-            },'json').error(function(){layer.msg('程序错误!');});
+            },'json').error(function(result){
+                if (result.responseJSON.echo){
+                    layer.msg(result.responseJSON.echo);
+                }else{
+                    layer.msg('程序错误!');
+                }
+            });
         });
         //监听是否首页
         form.on('checkbox(isHome)', function(data){
@@ -114,7 +120,13 @@
             }
             $.post('{{url("/Article/ajaxEdit")}}',{id:data.value,isHome:isHomeVal},function(result){
                 layer.msg(result.echo);
-            },'json').error(function(){layer.msg('程序错误!');});
+            },'json').error(function(result){
+                if (result.responseJSON.echo){
+                    layer.msg(result.responseJSON.echo);
+                }else{
+                    layer.msg('程序错误!');
+                }
+            });
         });
         //监听是否推荐
         form.on('checkbox(isRecommend)', function(data){
@@ -125,7 +137,13 @@
             }
             $.post('{{url("/Article/ajaxEdit")}}',{id:data.value,isRecommend:isRecommendVal},function(result){
                 layer.msg(result.echo);
-            },'json').error(function(){layer.msg('程序错误!');});
+            },'json').error(function(result){
+                if (result.responseJSON.echo){
+                    layer.msg(result.responseJSON.echo);
+                }else{
+                    layer.msg('程序错误!');
+                }
+            });
         });
         //监听单元格编辑
         table.on('edit(dataTable)', function(obj){
@@ -137,7 +155,13 @@
             data[obj.field] = obj.value;
             $.post('{{url("/Article/ajaxEdit")}}',data,function(result){
                 layer.msg(result.echo);
-            },'json');
+            },'json').error(function(result){
+                if (result.responseJSON.echo){
+                    layer.msg(result.responseJSON.echo);
+                }else{
+                    layer.msg('程序错误!');
+                }
+            });
         });
         //监听工具条点击
         table.on('tool(dataTable)', function(obj) {
@@ -154,7 +178,14 @@
                             obj.del(); //删除对应行（tr）的DOM结构
                             layer.close(index);
                         }
-                    },'json');
+                    },'json').error(function(result){
+                        layer.close(index);
+                        if (result.responseJSON.echo){
+                            layer.msg(result.responseJSON.echo);
+                        }else{
+                            layer.msg('程序错误!');
+                        }
+                    });
                 });
             }else if(layEvent === 'edit'){
                 layer.open({
@@ -234,7 +265,14 @@
                         form.render();
                         getDataTable();
                     }
-                },'json');
+                },'json').error(function(result){
+                    layer.close(index);
+                    if (result.responseJSON.echo){
+                        layer.msg(result.responseJSON.echo);
+                    }else{
+                        layer.msg('程序错误!');
+                    }
+                });
             });
         }
     </script>
