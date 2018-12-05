96c5414ea8f0f70d34bfa197a4bb45ee6be79258
user <248187189@qq.com>
2018-12-05 13:02:10 +0800

    1

--- resources/views/Admin/Login/login.blade.php
+++ resources/views/Admin/Login/login.blade.php
@@ -16,6 +16,9 @@
         body{
             background: #000;
             overflow:hidden;
+            background: url({{randGetBingEverydayImgForOnline()}}) no-repeat;
+        　　background-size:cover;
+        　　background-position: center center;
         }
         canvas{
             margin: 0 auto;
@@ -38,16 +41,16 @@
             Please&nbsp;&nbsp;Click&nbsp;&nbsp;Enter
         </div>
     </div>
-    <canvas id="canvas">
-        您的浏览器不支持canvas
-    </canvas>
+    {{--<canvas id="canvas">--}}
+        {{--您的浏览器不支持canvas--}}
+    {{--</canvas>--}}
 @endsection
 {{--js内容--}}
 @section('script')
     <script type="text/javascript">
-        //自定义验证
+        {{--自定义验证--}}
         form.verify({
-            account: function(value, item){ //value：表单的值、item：表单的DOM对象
+            account: function(value, item){ {{--value：表单的值、item：表单的DOM对象--}}
                 if(value.length<=2){
                     return '用户名不应该少于3位字符';
                 }
@@ -66,11 +69,11 @@
                 '密码必须6到16位，且不能出现空格'
             ]
         });
-        //监听登陆提交
+        {{--监听登陆提交--}}
         form.on('submit(login)', function (data) {
             var index = layer.load(1);
             $.ajax({
-                url:'{{ url('/doLogin') }}',// 跳转到 action
+                url:'{{ url('/doLogin') }}',
                 type:'post',
                 data:data.field,
                 cache:false,
@@ -99,15 +102,15 @@
             });
             return false;
         });
-        //检测键盘按下
+        {{--检测键盘按下--}}
         $('body').keyup(function (e) {
             if (e.keyCode == 13) {  //Enter键
                 if ($('#layer-login').length <= 0) {
-                    //登录框不存在，则显示登录框
+                    {{--登录框不存在，则显示登录框--}}
                     loginHtml();
                 } else {
-                    //判断三个输入框是否获取焦点，都没有获取焦点就模拟点击
-                    //如果获取了焦点，因为layui表单自动绑定了Enter键提交表单，所以不用再模拟点击一次
+                    {{--判断三个输入框是否获取焦点，都没有获取焦点就模拟点击--}}
+                    {{--如果获取了焦点，因为layui表单自动绑定了Enter键提交表单，所以不用再模拟点击一次--}}
                     var accountInputIsFocus =  $('input[name="account"]').is(':focus');
                     var passwordInputIsFocus =  $('input[name="password"]').is(':focus');
                     var verifyInputIsFocus =  $('input[name="verify"]').is(':focus');
@@ -117,11 +120,11 @@
                 }
             }
         });
-        //点击div出现登录框
+        {{--点击div出现登录框--}}
         $('.enter').click(function(){
             loginHtml();
         });
-        //登录框
+        {{--登录框--}}
         function loginHtml(){
             var loginHtml = '';
             loginHtml += '<form class="layui-form" action="">';
@@ -176,40 +179,40 @@
                 }
             });
         }
-        /**
-         * canvas动画
-         */
-        var canvas = document.getElementById(`canvas`), //获取画布
-            cxt = canvas.getContext(`2d`), //获得2d绘图环境
-            size = 12;//设置字体大小和列宽
-        //注意点：canvas不能在样式里设置宽高，那是对默认300*150的一个拉伸变形。
-        //可以直接在标签上设置宽高，如：width="500" height="500"(像图片标签那样)。
-        canvas.width = window.screen.width; //设置画布宽度
-        canvas.height = window.screen.height;//设置画布高度
-        var y = [], //用数组存储每一列更新后的y坐标
-            cols = canvas.width/size; //根据每一列宽为size来计算列数
-        //初始化每一列y值(随机)，取不同的值让雨点错开（不并排下落）
+        /*
+        {{--canvas动画--}}
+        var canvas = document.getElementById(`canvas`), {{--获取画布--}}
+            cxt = canvas.getContext(`2d`), {{--获得2d绘图环境--}}
+            size = 12;{{--设置字体大小和列宽--}}
+        {{--注意点：canvas不能在样式里设置宽高，那是对默认300*150的一个拉伸变形。--}}
+        {{--可以直接在标签上设置宽高，如：width="500" height="500"(像图片标签那样)。--}}
+        canvas.width = window.screen.width; {{--设置画布宽度--}}
+        canvas.height = window.screen.height;{{--设置画布高度--}}
+        var y = [], {{--用数组存储每一列更新后的y坐标--}}
+            cols = canvas.width/size; {{--根据每一列宽为size来计算列数--}}
+        {{--初始化每一列y值(随机)，取不同的值让雨点错开（不并排下落）--}}
         for(var i=0;i<cols;i++){
             y[i] = Math.random()*800;
         }
-        //画图函数
+        {{--画图函数--}}
         (function draw(){
-            //理解难点就是以下这个背景覆盖
-            cxt.fillStyle = `rgba(0,0,0,.1)`; //设置背景颜色和透明度（将会叠加，形成渐变）
-            cxt.fillRect(0,0,canvas.width,canvas.height); //描绘每一行背景
-            cxt.fillStyle = `#33ff00`; //设置字体颜色
-            cxt.font = `${size}px Microsoft yahei`; //设置字体
-            for(var i=0;i<cols;i++){//描绘所有列
-                var num = Math.floor(Math.random()*10)+'';//获取0-9随机数
-                var numX = i*size;//第i列的x坐标
-                var numY = y[i];//第i列的y坐标
-                cxt.fillText(num,numX,numY);//描绘数字
-                y[i] += size;//增加size高度以写该列中下一个（行）数字的y坐标
-                if(y[i]>=canvas.height && Math.random()>=.9){ //随机取值进一步控制雨点错开
-                    y[i] = 0; //回到顶部再写
+            {{--理解难点就是以下这个背景覆盖--}}
+            cxt.fillStyle = `rgba(0,0,0,.1)`; {{--设置背景颜色和透明度（将会叠加，形成渐变）--}}
+            cxt.fillRect(0,0,canvas.width,canvas.height); {{--描绘每一行背景--}}
+            cxt.fillStyle = `#33ff00`; {{--设置字体颜色--}}
+            cxt.font = `${size}px Microsoft yahei`; {{--设置字体--}}
+            for(var i=0;i<cols;i++){ {{--描绘所有列--}}
+                var num = Math.floor(Math.random()*10)+'';{{--获取0-9随机数--}}
+                var numX = i*size;{{--第i列的x坐标--}}
+                var numY = y[i];{{--第i列的y坐标--}}
+                cxt.fillText(num,numX,numY);{{--描绘数字--}}
+                y[i] += size;{{--增加size高度以写该列中下一个（行）数字的y坐标--}}
+                if(y[i]>=canvas.height && Math.random()>=.9){ {{--随机取值进一步控制雨点错开--}}
+                    y[i] = 0; {{--回到顶部再写--}}
                 }
             }
-            window.requestAnimationFrame(draw);//帧动画函数
+            window.requestAnimationFrame(draw);{{--帧动画函数--}}
         })();
+        */
     </script>
 @endsection
