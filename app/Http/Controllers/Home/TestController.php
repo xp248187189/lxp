e62f32bb791e00f804272091b1bac9906cc7daa7
yxiupei <30883503+yxiupei@users.noreply.github.com>
2020-06-28 17:20:13 +0800

    Update TestController.php

--- app/Http/Controllers/Home/TestController.php
+++ app/Http/Controllers/Home/TestController.php
@@ -1,7 +1,14 @@
 <?php
 namespace App\Http\Controllers\Home;
+use Carbon\Carbon;
+use App\Jobs\CountArticleComment;
+use App\Model\User;
+use App\Model\About;
+use App\Model\Link;
+use App\Model\UserComment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class TestController extends Controller{
   public function test(Request $request){
